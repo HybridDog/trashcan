55ca0ea5877710f876f01b78d41790fd0a8ddca0
HybridDog <adresse_test@t-online.de>
2015-09-27 16:03:21 +0200

    :cat2:

--- init.lua
+++ init.lua
@@ -2,9 +2,9 @@ local load_time_start = os.clock()
 local RANGE = 0.6
 
 minetest.register_node("trashcan:trashcan", {
-	description = "Trash can",
-	tiles = {"default_steel_block.png","default_steel_block.png","trashcan.png"},
-	groups = {snappy=1,bendy=2,cracky=1},
+	description = "steel trashcan",
+	tiles = {"default_steel_block.png", "default_steel_block.png", "trashcan.png"},
+	groups = {snappy=1, bendy=2, cracky=1},
 	sounds = default.node_sound_stone_defaults(),
 	paramtype = "light",
 	drawtype = "nodebox",
@@ -18,30 +18,33 @@ minetest.register_node("trashcan:trashcan", {
 			{7/16, -0.5, -0.5, 0.5, 0.5, 0.5},
 		},
 	},
-	on_punch = function(pos, node, puncher)
-		if node.name == "trashcan:trashcan" then --why should this be checked???
-			local objs = minetest.get_objects_inside_radius({x=pos.x, y=pos.y+0.3, z=pos.z}, RANGE)
-			minetest.chat_send_player(puncher:get_player_name(), 'Garbage removed!')
-			for _, obj in pairs(objs) do
-				obj:remove()
-				minetest.sound_play("trashcan", {pos = pos, max_hear_distance = 10})
-			end
-			minetest.log("info", "[trashcan] someone used a trashcan at ("..pos.x..", "..pos.y..", "..pos.z..")")
+	on_punch = function(pos, _, puncher)
+		minetest.chat_send_player(puncher:get_player_name(), "Garbage removed!")
+		for _,obj in pairs(minetest.get_objects_inside_radius({x=pos.x, y=pos.y+0.3, z=pos.z}, RANGE)) do
+			obj:remove()
+			minetest.sound_play("trashcan", {pos = pos, max_hear_distance = 10})
 		end
+		minetest.log("info", "[trashcan] someone used a trashcan at ("..pos.x.." | "..pos.y.." | "..pos.z..")")
 	end,
 	on_construct = function(pos)
-		local meta = minetest.get_meta(pos)
-		meta:set_string("infotext", "Trash can")
+		minetest.get_meta(pos):set_string("infotext", "Trash can")
 	end,
 })
 
 minetest.register_craft({
-	output = 'trashcan:trashcan',
+	output = "trashcan:trashcan",
 	recipe = {
-		{'default:steel_ingot', '', 'default:steel_ingot'},
-		{'default:steel_ingot', '', 'default:steel_ingot'},
-		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{"default:steel_ingot", "", "default:steel_ingot"},
+		{"default:steel_ingot", "", "default:steel_ingot"},
+		{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
 	}
 })
 
-minetest.log("info", string.format("[trashcan] loaded after ca. %.2fs", os.clock() - load_time_start))
+
+local time = math.floor(tonumber(os.clock()-load_time_start)*100+0.5)/100
+local msg = "[trashcan] loaded after ca. "..time
+if time > 0.05 then
+	print(msg)
+else
+	minetest.log("info", msg)
+end
